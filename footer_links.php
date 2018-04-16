506560286924e7c78d5edf3d07641c5d8ec2471c
chathu_wijesinghe <chathuwijesinghe00@gmail.com>
2018-04-16 09:34:19 +0530

    Chathuwata mokakda wela...

--- footer_links.php
+++ footer_links.php
@@ -3,5 +3,8 @@
     <script src="../js/jquery.min.js" type="text/javascript"></script>
     <script src="../js/tutorial.js"></script>
     <script src="../js/wow.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/picturefill/2.3.1/picturefill.min.js"></script>
+    <script src="../js/lightgallery-all.min.js"></script>
+    <script src="../js/jquery.mousewheel.min.js"></script>
     <script src="../js/script.js" type="text/javascript"></script>
 </footer>
--- header_links.php
+++ header_links.php
@@ -11,10 +11,17 @@
     <link href="../css/font-awesome.min.css" rel="stylesheet"/>
     <link href="../css/animate.css" rel="stylesheet"/>
     <link rel="stylesheet" type="text/css" href="../css/animate.css"/>
+    <link href="../css/lightgallery.css" rel="stylesheet">
     <link href="../css/styles.css" rel="stylesheet"/>
     <link href="../css/responsive.css" rel="stylesheet"/>
     <!----js---->
+
     <script src="../js/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script src="https://cdn.jsdelivr.net/picturefill/2.3.1/picturefill.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../js/lightgallery-all.min.js"></script>
+    <script src="../js/lightgallery.js"></script>
+    <script src="../js/jquery.mousewheel.min.js"></script>
     <script src="../js/bootstrap.min.js" type="text/javascript"></script>
     <script src="../js/script.js" type="text/javascript"></script>
 </head>
