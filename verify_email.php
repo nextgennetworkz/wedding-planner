919cf8a368cf48358538a12930cd3586b7e42203
chathu_wijesinghe <chathuwijesinghe00@gmail.com>
2018-04-18 09:13:27 +0530

    Magei Malshigei wedding page eka...

--- verify_email.php
+++ verify_email.php
@@ -8,14 +8,32 @@
 // Let's get the email to be verified
 $email = $_GET['email'];
 ?>
-<form id="verify_email_form" name="verify_email_form" action="verfiy_email_process.php" method="post">
-    <fieldset>
-        <legend>Verify your email</legend>
-        Email:
-        <input type="email" id="email" name="email" readonly="readonly" required="required"
-               value="<?php echo $email; ?>"><br>
-        Verification code:
-        <input type="text" id="activation_code" name="activation_code" required="required"><br>
-        <input type="submit" value="Verify">
-    </fieldset>
-</form>
+<?php require_once 'header_links.php'; ?>
+<?php require_once 'main_header.php'; ?>
+    <section class="signup-sec">
+        <div class="container">
+            <div class="col-sm-6 col-sm-offset-3">
+                <form id="sign_up" name="sign_up" action="sign_up.php" method="post">
+                    <div class="signup-content">
+                        <form id="verify_email_form" name="verify_email_form" action="verfiy_email_process.php" method="post">
+                            <fieldset>
+                                <legend><h2>Verify Your Email</h2></legend>
+
+                                <div class="input-field">
+                                    <input type="email" id="email" name="email" readonly="readonly" required="required"
+                                           value="<?php echo $email; ?>"><br>
+                                </div>
+                                <div class="input-field">
+                                    <input type="text" id="activation_code" placeholder="Verification Code" name="activation_code" required="required"><br>
+                                </div>
+
+                                <button>Verify</button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+<?php require_once 'footer_links.php'; ?>
